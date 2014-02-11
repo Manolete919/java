a6ba05044ecc343358f52633a6f446c41dd4527e
Desarrollo <Desarrollo@Desa41>
2014-02-11 14:46:47 -0500

    Creacion de Clase Nuevo Branch

---
+++ src/sbrpas/HelloWorld.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sbrpas;
+
+/**
+ *
+ * @author Desarrollo
+ */
+public class HelloWorld {
+    public void helloWorld(){
+        System.out.println("Hello World!!");
+    }
+}
