14a5d0df12f96e4f32d7e1334e411b8456db7257
Desarrollo <Desarrollo@Desa41>
2014-02-11 14:43:16 -0500

    nuevo commit2

---
+++ src/sbrpas/HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sbrpas;
+
+/**
+ *
+ * @author Desarrollo
+ */
+public class HolaMundo {
+    public void saludar(){
+        System.out.println("Hola mundo");
+    }
+}
